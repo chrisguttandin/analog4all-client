0dd4922922e994c77b784391f840fb3f5cf467c8
chrisguttandin <chrisguttandin@media-codings.com>
2016-12-22 02:02:35 +0100

    log travis config

--- config/grunt/if.js
+++ config/grunt/if.js
@@ -2,10 +2,14 @@ module.exports = {
     deploy: {
         ifTrue: [ 'deploy' ],
         options: {
-            test: () => process.env.TRAVIS === 'true' &&
+            test: () => {
+                console.log(process.env.TRAVIS, process.env.TRAVIS_PULL_REQUEST, process.env.TRAVIS_SECURE_ENV_VARS, process.env.TRAVIS_TAG); // eslint-disable-line no-console
+
+                return (process.env.TRAVIS === 'true' &&
                     process.env.TRAVIS_PULL_REQUEST === 'false' &&
                     process.env.TRAVIS_SECURE_ENV_VARS === 'true' &&
-                    process.env.TRAVIS_TAG !== ''
+                    process.env.TRAVIS_TAG !== '');
+            }
         }
     }
 };
