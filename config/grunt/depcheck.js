97096cdcdf4162b83cdf6743be2ae48b22f5ed89
Christoph Guttandin <chrisguttandin@media-codings.com>
2017-06-03 23:46:05 +0200

    add greenkeeper-lockfile to ignored depedencies

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -11,6 +11,7 @@ module.exports = {
                 '@angular/language-service',
                 '@types/*',
                 'eslint-config-holy-grail',
+                'greenkeeper-lockfile',
                 'grunt-*',
                 'husky',
                 'jasmine-core',
