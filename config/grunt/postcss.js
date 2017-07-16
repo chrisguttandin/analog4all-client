386d626e5829edc3eac67bfb1a7da24fd5ced0ea
chrisguttandin <chrisguttandin@media-codings.com>
2017-07-16 17:20:54 +0200

    use stylelint-config-holy-grail

--- config/grunt/postcss.js
+++ config/grunt/postcss.js
@@ -1,35 +1,12 @@
 const stylelint = require('stylelint');
+const stylelintConfigHolyGrail = require('stylelint-config-holy-grail');
 
 module.exports = {
     lint: {
         options: {
             processors: [
-                stylelint({
-                    rules: {
-                        'block-closing-brace-newline-after': 'always',
-                        'block-closing-brace-newline-before': 'always',
-                        'block-no-empty': true,
-                        'block-opening-brace-newline-after': 'always',
-                        'block-opening-brace-newline-before': 'never-single-line',
-                        'color-no-invalid-hex': true,
-                        'declaration-block-no-duplicate-properties': true,
-                        'declaration-block-no-shorthand-property-overrides': true,
-                        'declaration-block-semicolon-newline-after': 'always',
-                        'declaration-block-semicolon-space-before': 'never',
-                        'declaration-block-trailing-semicolon': 'always',
-                        'declaration-colon-space-after': 'always',
-                        'declaration-colon-space-before': 'never',
-                        'declaration-no-important': true,
-                        'indentation': 4,
-                        'length-zero-no-unit': true,
-                        'max-empty-lines': 1,
-                        'no-eol-whitespace': true,
-                        'number-leading-zero': 'always',
-                        'selector-list-comma-newline-after': 'always',
-                        'selector-list-comma-space-before': 'never',
-                        'string-quotes': 'single'
-                    }
-                })
+                // @todo This should use { extends: 'stylelint-config-holy-grail' } but that is not possible with grunt-postcss@0.8.0.
+                stylelint(stylelintConfigHolyGrail)
             ],
             writeDest: false
         },
