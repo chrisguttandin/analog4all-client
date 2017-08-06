95817fb5907ebabc58f6149f060741ea2b1b4c2c
chrisguttandin <chrisguttandin@media-codings.com>
2017-08-06 20:00:07 +0200

    use tsconfig-holy-grail

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -19,6 +19,7 @@ module.exports = {
                 'husky',
                 'jasmine-core',
                 'karma*',
+                'tsconfig-holy-grail',
                 'tslint',
                 'tslint-config-holy-grail',
                 'typescript',
