5a4ee3bef743576e7692ffcc04e546876a97846e
Christoph Guttandin <chrisguttandin@media-codings.com>
2017-10-14 04:44:35 +0200

    chore(config): update grunt config

--- config/grunt/postcss.js
+++ config/grunt/postcss.js
@@ -1,12 +1,10 @@
 const stylelint = require('stylelint');
-const stylelintConfigHolyGrail = require('stylelint-config-holy-grail');
 
 module.exports = {
     lint: {
         options: {
             processors: [
-                // @todo This should use { extends: 'stylelint-config-holy-grail' } but that is not possible with grunt-postcss@0.8.0.
-                stylelint(stylelintConfigHolyGrail)
+                stylelint({ config: { extends: 'stylelint-config-holy-grail' } })
             ],
             writeDest: false
         },
