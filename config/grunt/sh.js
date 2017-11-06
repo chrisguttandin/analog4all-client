3f778ca21af4d9c81ca9adbc69cc5f24d01e4453
chrisguttandin <chrisguttandin@media-codings.com>
2017-11-06 15:52:41 +0100

    disable build-optimizer

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,6 +1,6 @@
 module.exports = {
     analyze: {
-        cmd: 'ng build --aot --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
+        cmd: 'ng build --aot --build-optimizer false --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
     },
     build: {
         cmd: 'ng build --aot --base-href /analog4all-client --no-sourcemap --prod'
@@ -18,7 +18,7 @@ module.exports = {
         cmd: 'ng serve --port 6699'
     },
     preview: {
-        cmd: 'ng serve --aot --port 6699 --prod'
+        cmd: 'ng serve --aot --build-optimizer false --port 6699 --prod'
     },
     smoke: {
         cmd: 'IS_SMOKE_TEST=true ng e2e --serve false'
