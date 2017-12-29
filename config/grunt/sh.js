bce2153f7747b20dd36f126932e0da866b59c33b
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-29 19:15:56 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,9 +1,9 @@
 module.exports = {
     analyze: {
-        cmd: 'ng build --aot --build-optimizer false --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
+        cmd: 'ng build --aot --build-optimizer --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
     },
     build: {
-        cmd: 'ng build --aot --base-href /analog4all-client --build-optimizer false --no-sourcemap --prod --stats-json && webpack-stats-duplicates build/stats.json && rm build/stats.json'
+        cmd: 'ng build --aot --base-href /analog4all-client --build-optimizer --no-sourcemaps --prod --stats-json && webpack-stats-duplicates build/stats.json && rm build/stats.json'
     },
     continuous: {
         cmd: 'ng test'
@@ -15,10 +15,10 @@ module.exports = {
         cmd: 'ng lint --type-check'
     },
     monitor: {
-        cmd: 'ng serve --port 6699'
+        cmd: 'ng serve --aot --port 6699'
     },
     preview: {
-        cmd: 'ng serve --aot --build-optimizer false --port 6699 --prod'
+        cmd: 'ng serve --aot --build-optimizer --port 6699 --prod'
     },
     smoke: {
         cmd: 'IS_SMOKE_TEST=true ng e2e --serve false && sonarwhal https://chrisguttandin.github.io/analog4all-client && rm cdp.pid'
