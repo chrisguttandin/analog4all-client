16a671904194ffd1fa24da19d26186eac8522b9d
chrisguttandin <chrisguttandin@media-codings.com>
2018-02-06 13:41:37 +0100

    fix: use relative base-href with a trailing slash

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -3,7 +3,7 @@ module.exports = {
         cmd: 'ng build --aot --build-optimizer --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
     },
     build: {
-        cmd: 'ng build --aot --base-href https://chrisguttandin.github.io/analog4all-client --build-optimizer --no-sourcemaps --prod --stats-json && webpack-stats-duplicates build/stats.json && rm build/stats.json'
+        cmd: 'ng build --aot --base-href /analog4all-client/ --build-optimizer --no-sourcemaps --prod --stats-json && webpack-stats-duplicates build/stats.json && rm build/stats.json'
     },
     continuous: {
         cmd: 'ng test'
