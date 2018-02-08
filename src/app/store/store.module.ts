cd97d558aca87c238713ab247962092800dce0e5
chrisguttandin <chrisguttandin@media-codings.com>
2018-02-08 14:45:00 +0100

    import store devtools when running in dev mode

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { StoreModule as NgRxStoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // tslint:disable-line:no-implicit-dependencies
+import { environment } from '../../environments/environment';
 import { appReducer } from './store';
 
-@NgModule({
-    imports: [
+const imports = (environment.production) ?
+    [
         NgRxStoreModule.forRoot(appReducer)
-    ]
+    ] :
+    [
+        NgRxStoreModule.forRoot(appReducer),
+        StoreDevtoolsModule.instrument({
+          maxAge: 5
+        })
+    ];
+
+@NgModule({
+    imports
 })
 export class StoreModule { }
