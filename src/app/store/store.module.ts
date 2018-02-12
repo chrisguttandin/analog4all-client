897894bfcf61b5d7362b8c6f094016af73a3f016
chrisguttandin <chrisguttandin@media-codings.com>
2018-02-12 15:35:38 +0100

    test: use ngrx-store-freeze during development

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { StoreModule as NgRxStoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // tslint:disable-line:no-implicit-dependencies
+import { storeFreeze } from 'ngrx-store-freeze'; // tslint:disable-line:no-implicit-dependencies
 import { environment } from '../../environments/environment';
 import { appReducer } from './store';
 
@@ -9,7 +10,9 @@ const imports = (environment.production) ?
         NgRxStoreModule.forRoot(appReducer)
     ] :
     [
-        NgRxStoreModule.forRoot(appReducer),
+        NgRxStoreModule.forRoot(appReducer, {
+            metaReducers: [ storeFreeze ]
+        }),
         StoreDevtoolsModule.instrument({
             maxAge: 5
         })
