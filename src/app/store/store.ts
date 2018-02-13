f9343ad41c7872808d1d7fb67abcb32f7d3b3ea2
chrisguttandin <chrisguttandin@media-codings.com>
2018-02-14 00:54:13 +0100

    refactor: create barrel file for reducers

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,6 +1,6 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { IAppState } from './interfaces';
-import { instrumentsReducer } from './reducers/instruments';
+import { instrumentsReducer } from './reducers';
 
 export const appReducer: ActionReducerMap<IAppState> = {
     instruments: instrumentsReducer
