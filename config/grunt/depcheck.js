2bd839ef8fb20924e82f2399d6ff67540db35294
chrisguttandin <chrisguttandin@media-codings.com>
2018-03-05 02:51:31 +0100

    build(config): update depcheck configuration

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -18,6 +18,7 @@ module.exports = {
                 '@types/*',
                 'axe-core',
                 'bundle-buddy',
+                'commitizen',
                 'eslint',
                 'eslint-config-holy-grail',
                 'greenkeeper-lockfile',
