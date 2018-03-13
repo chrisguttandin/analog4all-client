28fb02e7546ede5ac9e8d2f5251bb65de1d30162
chrisguttandin <chrisguttandin@media-codings.com>
2018-03-13 16:35:16 +0100

    build(config): update depcheck config

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -15,6 +15,7 @@ module.exports = {
                 '@commitlint/cli',
                 '@commitlint/config-angular',
                 '@ngrx/store-devtools',
+                '@sonarwhal/*',
                 '@types/*',
                 'axe-core',
                 'bundle-buddy',
