688de1101ff3f157927ebb908f4c2d49d2800e7a
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-12-12 16:07:11 +0100

    test: update protractor config

--- config/protractor/e2e.js
+++ config/protractor/e2e.js
@@ -15,7 +15,7 @@ exports.config = {
 
     allScriptsTimeout: 11000,
 
-    baseUrl: (env.IS_SMOKE_TEST) ? 'https://chrisguttandin.github.io' : `http://localhost:${ projects[defaultProject].targets.serve.options.port }`,
+    baseUrl: (env.IS_SMOKE_TEST) ? 'https://chrisguttandin.github.io/analog4all-client' : `http://localhost:${ projects[defaultProject].targets.serve.options.port }`,
 
     directConnect: !!env.TRAVIS,
 
