c2b45e2896f3a1b097bbb8261aad521543a79368
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-04-03 15:37:02 +0200

    test: explicitly disable tracking through webhint

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -26,8 +26,8 @@ module.exports = {
     },
     smoke: {
         cmd: (env.TRAVIS) ?
-            "IS_SMOKE_TEST=true ng e2e --dev-server-target '' && hint https://chrisguttandin.github.io/analog4all-client && rm cdp.pid" :
-            "webdriver-manager update && IS_SMOKE_TEST=true ng e2e --dev-server-target '' --no-webdriver-update && hint https://chrisguttandin.github.io/analog4all-client && rm cdp.pid"
+            "IS_SMOKE_TEST=true ng e2e --dev-server-target '' && hint --tracking=off https://chrisguttandin.github.io/analog4all-client && rm cdp.pid" :
+            "webdriver-manager update && IS_SMOKE_TEST=true ng e2e --dev-server-target '' --no-webdriver-update && hint --tracking=off https://chrisguttandin.github.io/analog4all-client && rm cdp.pid"
     },
     test: {
         cmd: 'ng test --watch false'
