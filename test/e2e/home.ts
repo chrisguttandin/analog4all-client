9b91e96d500430802cf14227b0f043e38542b5aa
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-04-03 15:21:22 +0200

    test: verify that e2e test do not log any erros

--- test/e2e/home.ts
+++ test/e2e/home.ts
@@ -1,8 +1,27 @@
+import { browser, logging } from 'protractor';
 import { HomePage } from './home.po';
 
 describe('/', () => {
 
     let page: HomePage;
+
+    afterEach(async () => {
+        try {
+            // Assert that there are no errors emitted from the browser
+            const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+
+            expect(logs).not.toContain(jasmine.objectContaining(<Partial<logging.Entry>> {
+                level: logging.Level.SEVERE
+            }));
+        } catch (err) {
+            // @todo The driver for Safari does not support to retrieve the logs.
+            if (err.name === 'UnsupportedOperationError') {
+                console.warn('The driver for Safari does not support to retrieve the logs.'); // tslint:disable-line:no-console
+            } else {
+                throw err; // tslint:disable-line:rxjs-throw-error
+            }
+        }
+    });
 
     beforeEach(() => {
         page = new HomePage();
