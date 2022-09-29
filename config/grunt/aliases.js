751a724728ddd64a482fc864c451753dd7b76034
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-09-29 09:46:16 +0200

    build(config): update grunt config

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -4,7 +4,6 @@ module.exports = {
         'sh:build',
         'sh:verify',
         'clean:source-maps',
-        'replace:bundle',
         'copy:404',
         'replace:runtime',
         'clean:runtime',
