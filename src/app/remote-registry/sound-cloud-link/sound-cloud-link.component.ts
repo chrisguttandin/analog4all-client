ae11aeece6d6b26bcb9c18083188281f78d69a13
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-04-18 21:21:12 +0200

    style: remove empty css files

--- src/app/remote-registry/sound-cloud-link/sound-cloud-link.component.ts
+++ src/app/remote-registry/sound-cloud-link/sound-cloud-link.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'anc-sound-cloud-link',
-    styleUrls: ['./sound-cloud-link.component.css'],
     templateUrl: './sound-cloud-link.component.html'
 })
 export class SoundCloudLinkComponent {
