9abac9f96f50223bfc0b74d59e581a87fea11720
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-28 22:12:09 +0100

    build(config): use npm scripts for testing tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -7,23 +7,8 @@ module.exports = () => {
         'build': {
             cmd: 'npx ng build --base-href /analog4all-client/ --configuration production --subresource-integrity'
         },
-        'continuous': {
-            cmd: 'npx ng test'
-        },
-        'e2e': {
-            cmd: `npx playwright install --with-deps && \
-                npx playwright test --config config/playwright/config.ts`
-        },
         'rimraf-source-maps': {
             cmd: 'rimraf build/analog4all-client/browser/**.map'
-        },
-        'smoke': {
-            cmd: `npx playwright install --with-deps && \
-                IS_SMOKE_TEST=true npx playwright test --config config/playwright/config.ts && \
-                npx hint --telemetry=off https://chrisguttandin.github.io/analog4all-client`
-        },
-        'test': {
-            cmd: 'npx ng test --watch false'
         },
         'verify': {
             cmd: `npx bundle-buddy build/analog4all-client/browser/*.js.map && \
